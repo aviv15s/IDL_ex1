9a1b8b32d7be2123575e7b7185ba309b6b427bbd
ramGoldstein <ram.goldstein.43@gmail.com>
2024-05-28 13:45:48 +0300

    start

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-import torch
-import numpy as np
-from torchvision import datasets
+import os
 import pandas as pd
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+from torchvision.io import read_image
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
 
-if __name__ == "__main__":
-    
-    print("hello world")
+from torch.utils.data import TensorDataset, DataLoader
+# Example usage:
+file_path = 'neg_A0201.txt'
+with open(file_path, 'r') as file:
+    f = file.readlines()
+data_rows = [line.strip().split(' ') for line in f]
+tensor_y = torch.Tensor(data_rows)
+my_dataset = TensorDataset(tensor_y) # create your datset
+dataloader = DataLoader(my_dataset) #
+# Iterate through the DataLoader
+for batch in dataloader:
+    print(batch)
