85f9cd7195a42ae04209913bc81e0a3c7cf394b1
aviv15s <aviv.shemesh.43@gmail.com>
2024-05-28 10:52:47 +0300

    added files

--- dataloader.py
+++ dataloader.py
@@ -1,11 +1,19 @@
 import pandas as pd
 
-def load_data_from_txt_to_pd(file_path, data_name):
-    with open(file_path, 'r') as f:
-        lines = f.readlines()
+def load_data_from_txt_to_pd(pos_path, neg_path):
+    with open(pos_path, 'r') as f:
+        pos_lines = f.readlines()
+        pos_lines = [line.split for line in pos_lines]
 
-    lines = [line.split() for line in lines]
-    data = pd.DataFrame(lines)
+    with open(pos_path, 'r') as f:
+        neg_lines = f.readlines()
+        pos_lines = [line.split for line in neg_lines]
+
+
+    data = pd.DataFrame({
+        "acids": pos_lines,
+        "isPositive": [True] * len(pos_lines)
+    })
     print(data)
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import torch
+from torch.utils.data import Dataset, DataLoader, random_split
 import numpy as np
 from torchvision import datasets
 import pandas as pd
 
+def split_data(dataset, test_ratio):
+    test_size = test_ratio * len(dataset)
+    train_size = len(dataset) - test_size
+    train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+    return train_dataset, test_dataset
+
+
 if __name__ == "__main__":
-    
+
     print("hello world")
